4c9bc9bbe0bab253d1e0ae71e3e642257ed61408
artikov <oybekartikov@icloud.com>
2022-10-19 00:06:42 +0500

    Billing done

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { apple, bill, google } from '../assets'
-import styles, {layout} from '../style'
+import React from "react";
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../style";
 
 const Billing = () => (
   <section id="product" className={layout.sectionReverse}>
@@ -24,8 +24,20 @@ const Billing = () => (
         Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean
         neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.
       </p>
+      <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+        <img
+          src={apple}
+          alt="appstore"
+          className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
+        />
+        <img
+          src={google}
+          alt="google play"
+          className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
+        />
+      </div>
     </div>
   </section>
 );
 
-export default Billing
+export default Billing;
